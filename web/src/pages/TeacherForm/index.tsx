c7ddb4520d11b508dbe3386b196380d5d251b153
jainesenna <jainesantossenna191@gmail.com>
2020-08-07 16:28:46 -0300

    Alterando o index

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,68 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+import './styles.css';
+
+
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas." />
+      <PageHeader 
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher este formulário de inscrição" 
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus Dados</legend>
+
+          <Input name="name" label="Nome completo"/>
+          <Input name="avatar" label="Avatar"/>
+          <Input name="whatsapp" label="Whatsapp"/>
+          <Textarea name="bio" label="Biografia"/>
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobra a aula</legend>
+
+          <Select 
+              name="subject" 
+              label="Matéria"
+        
+              options={[
+                { value: 'Artes', label: 'Artes' },
+                { value: 'Biologia', label: 'Biologia' },
+                { value: 'Ciências', label: 'Ciências' },
+                { value: 'Educação Física', label: 'Educação Física' },
+                { value: 'Física', label: 'Física' },
+                { value: 'Geografia', label: 'Geografia' },
+                { value: 'História', label: 'História' },
+                { value: 'Matemática', label: 'Matemática' },
+                { value: 'Português', label: 'Português' },
+                { value: 'Química', label: 'Química' },
+              ]} 
+            />
+
+          <Input name="cost" label="Custo da sua hora por aula"/>
+        </fieldset>
+
+        <footer>
+          <p>
+          <img src={warningIcon} alt="Aviso importante" />
+              Importante! <br />
+              Preencha todos os dados
+          </p>
+          <button type="submit">
+              Salvar cadastro
+            </button>
+        </footer>
+        
+      </main>
     </div>
   )
 }
