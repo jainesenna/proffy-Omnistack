2afd7283357fb4ee2e70504be5c2661155aae11e
jainesenna <jainesantossenna191@gmail.com>
2020-08-06 22:08:35 -0300

    Update

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,14 @@
 import express, { request } from 'express';
 
-// import cors from 'cors';
-// import routes from './routes';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors());
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello World'});
-});
-// app.use(cors());
-
-// app.use(routes);
+app.use(routes);
 
 app.listen(3333);
