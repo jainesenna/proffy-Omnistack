6055a116c7624e3a10801ef43556a9ec9169b2e5
jainesenna <jainesantossenna191@gmail.com>
2020-08-08 19:05:52 -0300

    add mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontSize: 32,
+    color: '#F35',
+    fontWeight: 'bold'
+  }
+});
