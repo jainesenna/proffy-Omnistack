149373f9ca0703f1d944452c42dbbf5ccf372599
jainesenna <jainesantossenna191@gmail.com>
2020-08-08 13:40:29 -0300

    add history

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import Textarea from '../../components/Textarea';
@@ -11,6 +13,7 @@ import './styles.css';
 
 
 function TeacherForm() {
+  const history = useHistory();
 
   const [name, setName] = useState('');
   const [avatar, setAvatar] = useState('');
@@ -55,6 +58,8 @@ function TeacherForm() {
       schedule: scheduleItems
     }).then(() => {
       alert('Cadastro realizado com sucesso');
+
+      history.push('/');
     }).catch(() => {
       alert('Erro ao cadastrar');
     });
