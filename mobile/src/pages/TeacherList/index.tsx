ed5f399dd043d3f657ceabbaecf64236a114b3c9
jainesenna <jainesantossenna191@gmail.com>
2020-08-09 00:27:56 -0300

    alterando o index

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 
 import styles from './styles';
+import PageHeader from '../../components/PageHeader';
 
 function TeacherList() {
-  return <View style={styles.container}/>
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Proffys disponivéis" />
+    </View>
+  );
 }
-
 export default TeacherList;
