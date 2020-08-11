d54c963e1c99a25475f2eedb35e1d92ec0acf438
jainesenna <jainesantossenna191@gmail.com>
2020-08-10 22:27:36 -0300

    Add oprnURL Wpp

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, Linking } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
@@ -7,6 +7,7 @@ import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
 import styles from './styles';
+
 
 export interface Teacher {
   id: number;
@@ -24,6 +25,12 @@ interface TeacherItemProps {
 
 
 const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+
+  function handleLinkToWhatsapp() {
+    
+    Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
@@ -52,7 +59,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
             <Image source={unfavoriteIcon}/>
           </RectButton>
 
-          <RectButton style={styles.contactButton}>
+          <RectButton onPress={handleLinkToWhatsapp}style={styles.contactButton}>
             <Image source={whatsappIcon}/>
             <Text style={styles.contactButtonText}>Entar em contato</Text>
           </RectButton>
