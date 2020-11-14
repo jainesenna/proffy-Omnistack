421a992d11303484ec21ff62999b13ba57f68865
Jaine Senna <jainesantossenna191@gmail.com>
2020-11-14 15:15:37 -0300

    Add build

---
+++ web/build/precache-manifest.2a9306dc55dbd088923be69a2b23c119.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef076c0094475045a6203a0e48aa66c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9383822ff7d0853876fe",
+    "url": "/static/css/main.7ae5e58e.chunk.css"
+  },
+  {
+    "revision": "dd4a0a0c318cf0077ba5",
+    "url": "/static/js/2.2a2d43c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2a2d43c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9383822ff7d0853876fe",
+    "url": "/static/js/main.05f48526.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  },
+  {
+    "revision": "dc8c47fa945838a208074157d997436d",
+    "url": "/static/media/back.dc8c47fa.svg"
+  },
+  {
+    "revision": "156102ee75a899123483211bbf4a8fbd",
+    "url": "/static/media/give-classes.156102ee.svg"
+  },
+  {
+    "revision": "211f2bb00860f91804cfb2354e17b255",
+    "url": "/static/media/landing.211f2bb0.svg"
+  },
+  {
+    "revision": "86fef799b0726c66fb7a9d710e2d922c",
+    "url": "/static/media/logo.86fef799.svg"
+  },
+  {
+    "revision": "bfdb3ebe513b5b2411a5014d6aad3abe",
+    "url": "/static/media/purple-heart.bfdb3ebe.svg"
+  },
+  {
+    "revision": "e7a7e1b4e016245fa97e6f42aee4bcaa",
+    "url": "/static/media/study.e7a7e1b4.svg"
+  },
+  {
+    "revision": "6dfaa2f94fc54e53666a511b779f90b4",
+    "url": "/static/media/warning.6dfaa2f9.svg"
+  },
+  {
+    "revision": "45c6e6ecebde5ee441644d41cc132ce4",
+    "url": "/static/media/whatsapp.45c6e6ec.svg"
+  }
+]);
